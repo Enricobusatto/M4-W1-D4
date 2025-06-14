8f50c1cc1e1efd3436028f16e2ed67d343479fb1
Enricobusatto <enricobusatto75@gmail.com>
2025-06-14 12:17:22 +0200

    sistemato

--- fetch.js
+++ fetch.js
@@ -1,71 +1,87 @@
+const searchInput = document.getElementById("searchField");
+const found = document.getElementById("found");
+const searchSection = document.getElementById("searchSection");
 
-document.getElementById('btn-eminem').addEventListener('click', function () {
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem')
-        .then((response) => response.json())
-        .then((data) => {
-            const outputDiv = document.getElementById('eminem');
-            outputDiv.classList.remove('d-none');
-            outputDiv.classList.add('visibile');
-            const ul = document.createElement('ul');
-            const sectionDiv = document.getElementById('eminemSection');
+function search(){
+    const eminemSection = document.getElementById("eminem");
+    const metallicaSection = document.getElementById("metallica");
+    const queenSection = document.getElementById("queen");
+    eminemSection.classList = "d-none";
+    metallicaSection.classList = "d-none";
+    queenSection.classList = "d-none";
 
-            sectionDiv.appendChild(ul);
-
-
-            for (let i = 0; i < data.data.length; i++) {
-                const li = document.createElement('li');
-                li.textContent = data.data[i].title;
-                ul.appendChild(li);
-            }
-
-        })
-        .catch((error) => console.error(error));
-});
-
-document.getElementById('btn-queen').addEventListener('click', function () {
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=queen')
-        .then((response) => response.json())
-        .then((data) => {
-            const outputDiv = document.getElementById('queen');
-            outputDiv.classList.remove('d-none');
-            outputDiv.classList.add('visibile');
-            const ul = document.createElement('ul');
-            const sectionDiv = document.getElementById('queenSection');
-
-            sectionDiv.appendChild(ul);
+const ricerca = searchInput.value;
+fetchSongs(ricerca, true);
+}
 
 
-            for (let i = 0; i < data.data.length; i++) {
-                const li = document.createElement('li');
-                li.textContent = data.data[i].title;
-                ul.appendChild(li);
-            }
+function fetchSongs(artist, search = false){
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artist}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        renderSongs(data.data, artist, search);
+    })
+}
 
-        })
-        .catch((error) => console.error(error));
-});
-
-document.getElementById('btn-metallica').addEventListener('click', function () {
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica')
-        .then((response) => response.json())
-        .then((data) => {
-            const outputDiv = document.getElementById('metallica');
-            outputDiv.classList.remove('d-none');
-            outputDiv.classList.add('visibile');
-            const ul = document.createElement('ul');
-            
-            const sectionDiv = document.getElementById('metallicaSection');
-
-            sectionDiv.appendChild(ul);
+fetchSongs("Queen");
+fetchSongs("eminem");
+fetchSongs("Metallica");
 
 
-            for (let i = 0; i < data.data.length; i++) {
-                const li = document.createElement('li');
-                li.textContent = data.data[i].title;
-                ul.appendChild(li);
-            }
+function renderSongs(songs, artist, search = false) {
+    let artistSection = document.getElementById(`${artist}Section`);
+    let artistContainer = document.getElementById(artist);
 
-        })
-        .catch((error) => console.error(error));
-});
+    if (search) {
+        artistSection = searchSection;
+        artistContainer = found;
+    }
+
+
+    // svuota contenuto vecchio
+    artistSection.innerHTML = "";
+    for (let song of songs) {
+        const col = document.createElement("div");
+        col.className= "col mb-4";
+
+        const songCard = document.createElement("div");
+        songCard.className = "card h-100 shadow-sm";
+        col.appendChild(songCard);
+        
+        const songImg = document.createElement("img");
+        songImg.src = song.album.cover_medium;
+        songImg.className = "card-img-top img-fluid";
+        songImg.setAttribute("alt", song.title);
+        songCard.appendChild(songImg);
+
+        const songBody = document.createElement("div");
+        songBody.className = "card-body";
+        songCard.appendChild(songBody);
+        
+        const songTitle = document.createElement("h5");
+        songTitle.className = "card-title text-dark";
+        songTitle.textContent = song.title;
+        songBody.appendChild(songTitle);
+
+        const songAlbum = document.createElement("p");
+        songAlbum.className = "card-text text-secondary";
+        songAlbum.textContent = song.album.title;
+        songBody.appendChild(songAlbum);
+        artistSection.appendChild(col);
+
+        const audio = document.createElement("audio");
+        audio.className = "w-100";
+        audio.setAttribute("controls", "true");
+        audio.setAttribute("src", song.preview);
+        songBody.appendChild(audio);
+
+
+
+    }
+    // mostra sezione per l'artista
+    artistContainer.classList.remove("d-none");
+}
+    
+
 
